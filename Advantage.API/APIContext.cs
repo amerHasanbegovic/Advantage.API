e7eb767bad2c55638615c6bff843b250357a593e
Amer Hasanbegovic <amer.hasanbegovic@hotmail.com>
2020-10-17 22:32:37 +0200

    initial commit

---
+++ Advantage.API/APIContext.cs
@@ -0,0 +1,13 @@
+﻿using Advantage.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Advantage.API
+{
+    public class APIContext : DbContext
+    {
+        protected APIContext(DbContextOptions<APIContext> options) : base(options) { }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Server> Servers { get; set; }
+    }
+}
